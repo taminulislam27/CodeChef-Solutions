69cbf56c96bb09ee50195cb31ce0c563176aac09
Taminul islam <taminulislam27@gmail.com>
2026-01-22 01:59:05 +0600

    Added Candy_Types.cpp

---
+++ Starters 222 (Rated)/Candy_Types.cpp
@@ -0,0 +1,95 @@
+/*
+        /\_/\.
+       (= ._.)
+       / >  \>
+._____________________..
+|                      |
+|   Md_Taminul_Islam   |
+|   CSE @ BU           |
+|______________________|
+*/ \
+#include<bits/stdc++.h>
+using namespace std;
+#define faster()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << "\n"
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define ff first
+#define ss second
+ll gcdll(ll a, ll b) { return (b == 0 ? a : gcdll(b, a % b)); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+ll binpow(ll a, ll b)
+{ // a^b
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1)
+            res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ll binpow_mod(ll a, ll b, ll mod)
+{ // (a^b) % mod
+    ll res = 1 % mod;
+    a %= mod;
+    while (b)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+bool isPrime(ll n)
+{
+    if (n < 2)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    map<int, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        m[a[i]]++;
+    }
+    int mxC = -1;
+    for (auto x : m)
+        if (mxC < x.second)
+            mxC = x.second;
+    vector<int> b;
+    for (auto x : m)
+        if (mxC == x.second)
+            b.push_back(x.first);
+    sort(all(b));
+    cout << b[0] << endl;
+}
+int main()
+{
+    faster();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
