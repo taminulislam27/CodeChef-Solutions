7568c52a86f24cd21cc16508424f0a5f65baf334
Taminul islam <taminulislam27@gmail.com>
2026-02-06 21:00:56 +0600

    Added Qualified_or_Not.cpp

---
+++ Starters 224 (Rated)/Qualified_or_Not.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int a, b, c;
+    cin >> a >> b >> c;
+    (a >= 2 * b && a >= 2 * c) ? cout << "YES" : cout << "NO";
+}
