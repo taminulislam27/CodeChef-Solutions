ff0652f9c7fd2704c41001748cca33a20c941f0a
Taminul islam <taminulislam27@gmail.com>
2026-03-13 01:30:30 +0600

    Added Starters 229

---
+++ Starters 229/Sum_Not_Equal.cpp
@@ -0,0 +1,51 @@
+/*
+        /\_/\.
+       (= ._.)
+       / >  \>
+._____________________..
+|                      |
+|   Md_Taminul_Islam   |
+|   CSE @ BU           |
+|______________________|
+*/#include<bits/stdc++.h>
+using namespace std;
+#define faster()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define for_i(a, b) for(int i = a; i < b; i++)
+#define for_n(a, b) for(int i = a; i >= b; i--)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << "\n"
+#define ll long long
+void tamin()
+{
+    int n; cin>>n; 
+    vector<pair<int, int>>a(n); 
+    for (int i = 0; i < n; i++)
+    {
+        int x; cin>>x; 
+        a[i].first = x; 
+        a[i].second = i+1; 
+    }
+    sort(rall(a)); 
+    if(a[0].first == 0) cout<<-1<<endl; 
+    else
+    {
+        cout<<a[0].second<<" "; 
+        cout<<a[1].second<<" "; 
+        cout<<a[n-1].second<<endl; 
+    }
+}
+int main()
+{
+    faster();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        tamin();
+
+    return 0;
+}
